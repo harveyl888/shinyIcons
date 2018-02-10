d61219a3437389954f00c94ce52eb7f72c80814a
Harvey Lieberman <harvey.lieberman@gmail.com>
2018-02-10 12:08:34 -0500

    typo

--- R/zzz.R
+++ R/zzz.R
@@ -3,4 +3,4 @@
   shiny::addResourcePath('materialIcons', system.file('www', package = 'materialIcons'))
 }
 fa_dep <- htmltools::htmlDependency(name = 'fontAwesome', version = '5.0.6', src = c(href='shinyIcons/fontawesome'), script = 'fontawesome-all.js')
-md_dep <- htmltools::htmlDependency(name = 'MaterialIcons', version = '3.0.1', src = c(href='materialIcons/material_icons'), stylesheet = 'material-icons.css')
+md_dep <- htmltools::htmlDependency(name = 'MaterialIcons', version = '3.0.1', src = c(href='shinyIcons/material_icons'), stylesheet = 'material-icons.css')
